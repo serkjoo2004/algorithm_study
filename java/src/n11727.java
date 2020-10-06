7531200b0b9a3d1f9931b3dbcda7ce77a002fdf6
박석주 <seokju@seokju-MacBookAir.local>
2020-10-06 17:31:00 +0900

    delete

---
+++ java/src/n11727.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class n11727{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        int [] arr = new int [1001];
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for(int i=2; i<=1000;i++){
+            arr[i] = ((arr[i-2] * 2) + arr[i-1]) % 10007;
+        }
+
+        System.out.println(arr[sc.nextInt()]);
+    }
+}
