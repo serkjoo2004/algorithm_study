0a112de9630fa570f389deadbf09ceeba464e643
박석주 <seokju@seokju-MacBookAir.local>
2020-10-08 18:56:39 +0900

    dp exer

--- java/src/Main.java
+++ java/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String [] args){
-        int a =100000000;
+        int a =200000000;
         System.out.println(a);
     }
 }
