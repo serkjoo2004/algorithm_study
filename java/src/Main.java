ba5afeff5ced793606f85d9242b39e9e0eee29af
박석주 <seokju@seokju-MacBookAir.local>
2020-10-19 18:12:56 +0900

    DP

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main{
     static int num;
-    static int [] arr;
 
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
