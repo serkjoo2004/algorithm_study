6881a9d5d3b39e859ee736da9e441c7243caec1d
박석주 <seokju@seokju-MacBookAir.local>
2020-11-05 17:30:47 +0900

    DP

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,25 +1,5 @@
-import java.util.Scanner;
-
 public class Main{
-    static int A,B,C;
-
     public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        A = sc.nextInt();
-        B = sc.nextInt();
-        C = sc.nextInt();
-
-        long temp = A % C;
-        long ans = 1;
-        while(B>1){
-            if(B%2 == 1){
-                ans = (ans * (temp%C))%C;
-            }
-            temp = (temp * temp) % C;
-            B = B/2;
-        }
-
-        System.out.println(((ans)*temp)%C);
+        System.out.println('2' - '0');
     }
 }
-
---
+++ java/src/n2011.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class n2011{
+    static String num;
+    static long [] cost;
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextLine();
+        int mod = 1000000;
+        cost = new long[num.length() + 1];
+
+        cost[0] = cost[1] = 1;
+
+
+        if(num.charAt(0) == '0'){
+            System.out.println(0);
+            return;
+        }
+
+
+        for(int i=2; i<=num.length(); i++){
+            if(num.charAt(i-1) > '0'){
+                cost[i] = cost[i-1];
+            }
+            int temp = (num.charAt(i-2) -'0')*10 + num.charAt(i-1) -'0';
+            if(temp>=10 && temp<=26){
+                cost[i] = (cost[i] + cost[i-2]) % mod;
+            }
+        }
+
+        System.out.println(cost[cost.length-1]);
+    }
+}
